02f25edc36d36cd521fb557911b07bdda120ef77
Ramon Lago Alves Freire <ramonlago@outlook.com>
2020-06-20 19:49:42 -0300

    doing ho win function and change text colors.

--- public/function.js
+++ public/function.js
@@ -23,14 +23,21 @@ function drawScrenary(){
 }
 function drawTexts(){
   //Configure text layout
-  fill(COLORS.WHITE);
   textSize(16);
   //This player points
+  fill(COLORS.GREEN);
   text(player.getPoints(), (windowWidth/2)-50, 50);
   //Enemy points
+  fill(COLORS.RED);
   text(enemy.getPoints(), (windowWidth/2)+50, 50);
   //Velocity text
+  fill(COLORS.YELLOW);
   text("Velocity: "+ball.getVel(), (windowWidth/2)-120, windowHeight-50);
+}
+function hoWin(){
+  if(player.getPoints() >= 3){
+  }else if(enemy.getPoints() >= 3){
+  }
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
