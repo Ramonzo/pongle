8e24151829a5ddd1afd245042bc3810c3deb8b2d
Ramon Lago Alves Freire <ramonlago@outlook.com>
2020-06-19 22:07:50 -0300

    upload css file

--- public/function.js
+++ public/function.js
@@ -1,5 +1,11 @@
 function drawScrenary(){
   background(0);
+  noStroke();
+  rectMode(CENTER);
+  
+  //Draw center bar
+  fill(255);
+  rect(windowWidth/2, windowHeight/2, 20, windowHeight);
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
